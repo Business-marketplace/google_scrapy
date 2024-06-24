cb6674454eb42df42a3c226125b269e8860482cb
Shoukath Ali <shoukath.shaik@geekyinsightsstudio.com>
2024-06-24 03:43:00 +0000

    Google scrapy __ Resolve to eliminate  People also ask

--- google_search/spiders/google.py
+++ google_search/spiders/google.py
@@ -6,6 +6,7 @@ class GoogleSpider(scrapy.Spider):
     start_urls = ['https://google.com/search?q=meal+prep+plan+in+texas']
     
     custom_settings = {
+        'ROBOTSTXT_OBEY':False,
         'USER_AGENT': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
     }
 
